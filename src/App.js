dab9a380e37e133420288408bf28ae61d6d8237b
Shaktidhar <sdmohapatra7@gmail.com>
2023-05-21 19:58:15 +0530

    couner count

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+
 import './App.css';
 
 function App() {
+  const initialCount = 0;
+  const [count,setCount] = useState(initialCount);
+  const handleIncrease = ()=>{
+    // setCount(count + 1);
+    setCount((prevState)=>prevState + 1);
+  }
+  const handleDecrease = ()=>{
+    setCount((prevState)=>prevState - 1);
+  }
+  const handleReset = ()=>{
+    setCount(initialCount);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter</h1>
+      <button onClick={handleIncrease}> Increase </button>
+      <button onClick={handleDecrease}> Decrease </button>
+      <button onClick={handleReset}> Reset </button>
+      {count}
     </div>
   );
 }
