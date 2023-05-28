a0a203593027b655717166869f1ad3ac2f2e7ab5
Shaktidhar <sdmohapatra7@gmail.com>
2023-05-28 09:28:55 +0530

    score-keeper component to show the cricket score

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,37 +1,55 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+let ballWiseResult = [];
+let Initialscore = 0;
+let Initialwicket = 0;
 
-const Score =()=>{
-
-    let Initialscore = 0;
-    let Initialwicket =0;
-    const[score,setScore] = useState(Initialscore);
-    const[wicket,setWicket]= useState(Initialwicket);
+const Score = () => {
+    const [score, setScore] = useState(Initialscore);
+    const [wicket, setWicket] = useState(Initialwicket);
     // const addOne = ()=>{
     //     setScore(score + 1);
     //     // console.log('score',score);
     // }
-    const addScore = (num)=>{
-        if(wicket<10){
-            setScore(score+num);
+
+    const addScore = (num) => {
+        if (wicket < 10) {
+            ballWiseResult.push(num);
+            // setScore(score+num);
+            setScore((prevState) => prevState + num);
+            console.log(ballWiseResult);
         }
     }
-    const addWicket = ()=>{
-        if(wicket<10){
-            setWicket(wicket+1)
+    const addWicket = () => {
+        if (wicket < 10) {
+            ballWiseResult.push('W');
+            setWicket(wicket + 1);
+            console.log(ballWiseResult);
         }
-        
+
     }
-    return(
+    // console.log(ballWiseResult);
+    return (
         <>
-        <h1>SCORE KEEPER</h1>
-        <h2>SCORE:{score}/{wicket}</h2>
-        <button onClick={()=>addScore(1)}>1</button>
-        <button onClick={()=>addScore(2)}>2</button>
-        <button onClick={()=>addScore(3)}>3</button>
-        <button onClick={()=>addScore(4)}>4</button>
-        <button onClick={()=>addScore(5)}>5</button>
-        <button onClick={()=>addScore(6)}>6</button>
-        <button onClick={addWicket}>Wicket</button>
+            <div>
+                <h1>SCORE KEEPER</h1>
+                <h2>SCORE:{score}/{wicket}</h2>
+                <button onClick={() => addScore(0)}>0</button>
+                <button onClick={() => addScore(1)}>1</button>
+                <button onClick={() => addScore(2)}>2</button>
+                <button onClick={() => addScore(3)}>3</button>
+                <button onClick={() => addScore(4)}>4</button>
+                <button onClick={() => addScore(5)}>5</button>
+                <button onClick={() => addScore(6)}>6</button>
+                <button onClick={addWicket}>Wicket</button>
+            </div>
+            <div>
+                {ballWiseResult.map((res, index) => (
+                    <>
+                        {index % 6 === 0 ? <br /> : null}
+                        <span key={index}>{res === 0 ? <strong>.</strong> : res}</span>&nbsp;&nbsp;&nbsp;
+                    </>
+                ))}
+            </div>
         </>
     )
 }
